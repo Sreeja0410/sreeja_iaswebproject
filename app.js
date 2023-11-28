97ee41e2ced33f93e1a566240699ca67808885db
sreeja <gaddameedisreeja74@gmail.com>
2023-11-28 17:01:05 +0530

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const ejs=require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const bcrypt = require("bcryptjs")
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const passport = require("passport");
+
+const app = express();
+
+const db= require("./config/keys.js").MongoURI;
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+mongoose.connect(db)
+.then(() => console.log("alll ok"))
+.catch(err => console.log(err));
+
+app.get("/", function(req,res){
+   res.render("login");
+})
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+})
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb+srv://iasweb:Aj337Gcvjb36954@cluster0.46sjgij.mongodb.net/?retryWrites=true&w=majority"
+}
